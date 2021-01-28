01f8e223f4cac217bb4744c11cbbd09f31ba20b7
Gustavo Acosta <gustavoacosta741@hotmail.com>
2021-01-28 14:06:59 -0400

    seccion 5 , expandir nuestras bases

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
+import { DbzModule } from './dbz/dbz.module';
 
 
 
@@ -14,7 +15,8 @@ import { HeroesModule } from './heroes/heroes.module';
   ],
   imports: [
     BrowserModule,
-    HeroesModule
+    HeroesModule,
+    DbzModule
 
   ],
   providers: [],
